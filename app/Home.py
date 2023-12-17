78b8892da575e5bd3f3dc7a0d37168d9c5cc0d88
prammmoe <ikhwanpramuditha05@gmail.com>
2023-12-17 19:25:09 +0700

    feat: add 2 models

---
+++ app/Home.py
@@ -0,0 +1,9 @@
+# Library
+import numpy as np
+import cv2
+from keras.models import load_model
+import tensorflow as tf
+import streamlit as st
+
+# Define the app title
+st.title('Banana Leaf Disease Classification App')
